140b7b707023f3832687efaa5e94cd23d67af90d
Matthias Le Brun <mlbli@me.com>
2015-03-31 11:45:46 +0200

    added object-assign for testing

--- webpack.test.js
+++ webpack.test.js
@@ -1,2 +1,8 @@
+import assign from "object-assign"
+
+if(typeof Object.assign !== "function") {
+  Object.assign = assign
+}
+
 var context = require.context("./src", true, /__tests__\/\S+\.js$/)
 context.keys().forEach(context)
